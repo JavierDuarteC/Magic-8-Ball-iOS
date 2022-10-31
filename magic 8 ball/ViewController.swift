03748da0cced987e00b4a89a66880dcddb2eec06
Javier Duarte <javierandresduarte@gmail.com>
2022-10-31 15:03:04 -0500

    Ball show functionality

--- magic 8 ball/ViewController.swift
+++ magic 8 ball/ViewController.swift
@@ -11,15 +11,27 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var answerImageView: UIImageView!
     
+    private let ballAnswers = [
+        UIImage(named: "ball1"),
+        UIImage(named: "ball2"),
+        UIImage(named: "ball3"),
+        UIImage(named: "ball4"),
+        UIImage(named: "ball5")
+    ]
+    
     @IBAction func askButtonPressed(_ sender: Any) {
-        
+        answerImageView.image = nil
+        DispatchQueue.main.async {
+            let timeInterval = TimeInterval.random(in:0.3...0.5)
+            let maxIndex = self.ballAnswers.count-1
+            Thread.sleep(forTimeInterval: timeInterval)
+            self.answerImageView.image = self.ballAnswers[Int.random(in: 0...maxIndex)]
+        }
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        answerImageView.image = UIImage(named: "ball3")
     }
-
 
 }
 
